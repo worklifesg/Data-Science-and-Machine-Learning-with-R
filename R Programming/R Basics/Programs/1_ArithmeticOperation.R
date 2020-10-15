1099c8d755a4953e40af05e103b422962a4c4dcb
Shraman Gupta <worklife_sg@outlook.com>
2020-10-15 11:57:04 -0400

    Create 1_ArithmeticOperation.R

---
+++ R Programming/R Basics/Programs/1_ArithmeticOperation.R
@@ -0,0 +1,16 @@
+# In this code, basic arithmetic operations are presented using R
+
+cat("Addition Results are:\n",
+    1+2, 100+11, 12.2+ 13.1,
+    "\nSubtraction Results are:\n",
+    5 - 3, 15.5 - 0.5,
+    "\nDivision Results are:\n",
+    2/3, 1/2,
+    "\nPower of Results are:\n",
+    2^3, 4^4,
+    "\nBODMAS of Results are:\n",
+    100*2+50/2, 100*(2+50)/2,
+    "\nModular (Division Remainder) Results are:\n",
+    5/2, 5%%2, 10/8, 10%%8,
+    file = "myfile.txt")
+
